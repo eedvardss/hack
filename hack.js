e0c5bfee755f958be1fce9290a4aba48f990ce4f
eedvardss <31999288+eedvardss@users.noreply.github.com>
2025-11-26 02:26:19 +0200

    Update and rename hack.js to main.html

--- hack.js
+++
@@ -1,4 +0,0 @@
-alert("HACKED BY NIR!");
-document.body.style.background = "black";
-document.body.style.color = "lime";
-document.body.innerHTML = "<h1>GAME OVER</h1><marquee>YOUR SITE BELONGS TO ME NOW</marquee>";
