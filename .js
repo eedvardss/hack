71bb1461ca45d0b6b210023ad49ca9a5b1cedd62
eedvardss <31999288+eedvardss@users.noreply.github.com>
2025-11-26 02:08:14 +0200

    Modify alert and style for hack message

--- .js
+++ .js
@@ -1,1 +1,4 @@
-alert("I have full control!");
+alert("HACKED BY NIR!");
+document.body.style.background = "black";
+document.body.style.color = "lime";
+document.body.innerHTML = "<h1>GAME OVER</h1><marquee>YOUR SITE BELONGS TO ME NOW</marquee>";
